403b6e2e0a749be0ad3c98acd33996d33a878684
wartakita <161806668+wartakita@users.noreply.github.com>
2025-06-06 14:06:33 +0700

    Create video.js

---
+++ video.js
@@ -0,0 +1,14 @@
+[
+  {
+    "title": "GP 1",
+    "url": "https://jwplayerdash.blogspot.com/?url=aHR0cHM6Ly9xcC1wbGR0LWxpdmUtZ3JwLTEzLXByb2QuYWthbWFpemVkLm5ldC9vdXQvdS9kcl9zcG90djJoZC5tcGQ=&k1=N2VlYTcyZDYwNzUyNDVhOTllZTMyNTU2MDNkNTg4NTM=&k2=Njg0OGVmNjA1NzU1NzliZjRkNDE1ZGIxMDMyMTUzZWQ="
+  },
+  {
+    "title": "CH 2",
+    "url": "https://topembed.pw/channel/TNTSports2[UK]"
+  },
+  {
+    "title": "CH 3",
+    "url": "https://stream196tp.com/global1.php?stream=tnt_2_gb"
+  }
+]
